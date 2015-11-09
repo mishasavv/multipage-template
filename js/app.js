fcb22c5b5558ff5ab857d7937fb20c60d42d4e5b
mishasavv <mikhail3@uw.edu>
2015-11-09 09:50:09 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 // Create app
-
+var myApp = angular.module('myApp', ['ui.router']);
 // Configure app
+myApp.config(function($stateProvider) {
+    $stateProvider
+        .state('home', {
+            url:'',
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController',
+        })
+        .state('content', {
+            url:'content',
+            templateUrl: 'templates/content.html',
+            controller: 'ContentController',
+        })
+        .state('about', {
+            url:'about',
+            templateUrl: 'templates/about.html',
+            controller: 'AboutController',
+        })
+})
 
+myApp.controller('AboutController', function($scope){
+  $scope.about="hi";
+})
+myApp.controller('HomeController', function($scope){
+  $scope.number = "3";
+})
+myApp.controller('ContentController', function($scope){
+    $scope.url="http://ladiesloot.com/wp-content/uploads/2015/05/smiley-face-1-4-15.png";
+})
 // Landing page controller: define $scope.number as a number
 
 
